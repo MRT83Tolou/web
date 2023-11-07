fe324a889aeac35b591698e79e5b2bf7d0924c29
MRT83Tolou <147505240+MRT83Tolou@users.noreply.github.com>
2023-11-07 06:02:31 -0800

    obj response s06

--- s06.js
+++ s06.js
@@ -1,11 +1,43 @@
 let webpage = require('http');
 let server = webpage.createServer(Bot);
-server.listen(80);
+let port=(80);
+server.listen(port);
+console.log("server is running on port: "+ port);
+
+
 let header = {'Content-Type' : 'Text/Plain'}
+
 function Bot (request,response){
-    console.log('request recieved url: ',request.url);
+
+    //console.log('request recieved url: ',request.url);
     //console.log('request : ',request);
-    response.writeHead(200,header);
-    response.write('salam'+ request.url);
-    response.end();
+
+    let obj = { 
+        m: function(){
+            console.log("Mim mesle Madar")
+            response.writeHead(200,header);
+            response.write("Mim mesle Madar");
+            response.end();
+        },
+        a: function(){
+            console.log("A mesle atefe")
+            response.writeHead(200,header);
+            response.write("a mesle atefe");
+            response.end();
+        },
+        "favicon.ico":function(){
+            console.log("favicon")
+            response.writeHead(200,header);
+            response.write("favicon");
+            response.end();
+        }
+    }
+    
+    let firstPart = request.url.split('/')[1];
+    obj[firstPart]();
+
+    // response.writeHead(200,header);
+    // response.write(obj);
+    // response.end();
+
 }
